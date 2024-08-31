887caa3125379eb366eeedd4006dad173b00f8b8
Ahnaf <159417805+ahnaf505@users.noreply.github.com>
2024-08-31 18:10:05 +0700

    change main.py

--- main.py
+++ main.py
@@ -1,144 +1,249 @@
-print("Initializing...")
-from db import *
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox, simpledialog
 import pickle
-import sys
-import json
+from PIL import Image, ImageTk
+import sys, json, os
+from db import *
 from compare import *
-isvalidfile = check_filestrcture()
-if isvalidfile:
-    print("Proper file structure detected!")
-else:
-    print("Invalid file structure detected!")
-    print("Fixing file structure...")
 
-# Simple debug menu.
-print("FaceDentify\n")
-print("1. Insert new single face into database")
-print("2. Insert a lists of faces into database")
-print("3. Query face from biometric recognition")
-print("4. Query face from Face ID")
-print("5. Delete face from biometric recognition")
-print("6. Delete face from Face ID")
-print("7. Clear entire database CAUTION!!! REALLY DANGEROUS OPERATION")
+class ImageTextDialog(simpledialog.Dialog):
+    def __init__(self, parent, title=None, image_path=None, text=None):
+        self.image_path = image_path
+        self.text = text
+        self.image_label = None
+        self.max_width = 400
+        self.max_height = 400
+        super().__init__(parent, title)
 
-# User menu input field.
-menu = input("Please select menu ==> ")
+    def body(self, master):
+        if self.image_path:
+            self.original_image = Image.open(self.image_path)
+            self.image_label = tk.Label(master)
+            self.image_label.pack(pady=10)
 
-if menu == "1":
-    filepath = input("\nInput image candidate filename ==> ")
-    faceencode = encode_face(filepath)
-    if str(faceencode) == "False":
-        print("No face detected in image or invalid image file!")
-        sys.exit()
-    face_uuid = gen_id()
-    save_face(faceencode, face_uuid)
-    fullname = input("Please input subject's Full Name ==> ")
-    new_face(face_uuid, fullname)
-    save_copy(filepath, face_uuid)
-    print(f"New face added to the database with ID: {face_uuid}, and Full Name: {fullname}")
+        if self.text:
+            text_label = tk.Label(master, text=self.text)
+            text_label.pack(pady=10)
 
-elif menu == "2":
-    print("Please format the json file using the following format:")
-    print("""   [
-        {
-            "full_name": "Will Smith",
-            "filepath": "directory/9400248.png"
-        },
-        {
-            "full_name": "Will John",
-            "filepath": "directory/9489848.png"
-        },
-        {
-            "full_name": "Willyam Dan",
-            "filepath": "directory/9499048.png"
-        }
-    ]\n\n""")
-    jsonfilemname = input("JSON filename ==> ")
-    with open(jsonfilemname, 'r') as f:
-        peoples = json.load(f)
-    
-    for i, people in enumerate(peoples):
-        print(f"Processing json file record number: [{i+1}]")
-        faceencode = encode_face(people['filepath'])
-        if str(faceencode) == "False":
-            print(f"No face detected in image or invalid image file!, Full Name: {people['full_name']}")
-            sys.exit()
-        face_uuid = gen_id()
-        save_face(faceencode, face_uuid)
-        fullname = people['full_name']
-        new_face(face_uuid, fullname)
-        save_copy(people['filepath'], face_uuid)
-        print(f"\nNew face added to the database with ID: {face_uuid}, and Full Name: {fullname}")
-elif menu == "3":
-    filename = input("\nInput candidate image file name ==> ")
-    facencode = encode_face(filename)
-    if str(facencode) == "False":
-        print("No face detected in image or invalid image file!")
-        sys.exit()
-    
-    resultfound = False
-    faces_dir = os.listdir("facedb")
-    print("Searching db for similar faces...")
-    for facedir in faces_dir:
-        with open("facedb/"+facedir, 'rb') as f:
-            currencode = pickle.load(f)
-        if comparewithtuple(filename, currencode) == True:
-            print(f"Matched with Face ID:{facedir[:-5]} Searching for full name...")
-            print(f"Matched Face ID: {facedir[:-5]} with Full Name: {search_fullname(facedir[:-5])[0]['fullname']}")
-            print(f"Database face image on path: imgfacedb/{facedir[:-5]}.png")
-            resultfound = True
-            break
-    if resultfound == False:
-        print("No result found after going through the entire database!")
-    
-elif menu == "4":
-    faceid = input("Input Face ID ==> ")
-    if search_fullname(faceid):
-        print(f"Full Name: {search_fullname(faceid)[0]['fullname']}")
-        print(f"Raw face image path: faceimgdb/{search_fullname(faceid)[0]['faceid']}.png/jpg")
-    else:
-        print("No record found from the given ID!")
+        self.update_image(self.max_width, self.max_height)
+        master.bind("<Configure>", self.resize_event)
 
-elif menu == "5":
-    filename = input("\nInput candidate image file name ==> ")
-    facencode = encode_face(filename)
-    if str(facencode) == "False":
-        print("No face detected in image or invalid image file!")
-        sys.exit()
-    
-    resultfound = False
-    faces_dir = os.listdir("facedb")
-    print("Searching db for similar faces...")
-    for facedir in faces_dir:
-        with open("facedb/"+facedir, 'rb') as f:
-            currencode = pickle.load(f)
-        if comparewithtuple(filename, currencode) == True:
-            print(f"Matched with Face ID:{facedir[:-5]} Searching for full name...")
-            print(f"Matched Face ID: {facedir[:-5]} with Full Name: {search_fullname(facedir[:-5])[0]['fullname']}")
-            resultfound = True
-            print("Deleting data related to the detected face...")
-            deleteface(facedir[:-5])
-            print("Succesfully deleted all data related to the face!")
-            break
-    if resultfound == False:
-        print("No result found after going through the entire database!")
+    def update_image(self, width, height):
+        if self.image_label:
+            aspect_ratio = self.original_image.width / self.original_image.height
 
-elif menu == "6":
-    faceid= input("Input Face ID to delete ==> ")
-    deleteface(faceid)
-    print("Succesfully deleted all data related to the face!")
+            new_width = min(self.max_width, self.original_image.width)
+            new_height = int(new_width / aspect_ratio)
 
-elif menu == "7":
-    print("REALLY DANGEROUS OPERATION, THIS CANNOT BE UNDONE.")
-    isdelete1 = input("Please type '9840nfd9' to continue ==> ")
-    if isdelete1 == "9840nfd9":
+            if new_height > self.max_height:
+                new_height = self.max_height
+                new_width = int(new_height * aspect_ratio)
+
+            resized_image = self.original_image.resize((new_width, new_height), Image.Resampling.LANCZOS)
+            photo = ImageTk.PhotoImage(resized_image)
+            
+            self.image_label.config(image=photo)
+            self.image_label.image = photo
+
+    def buttonbox(self):
+        box = tk.Frame(self)
+        ok_button = tk.Button(box, text="OK", width=10, command=self.ok, default=tk.ACTIVE)
+        ok_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.bind("<Return>", self.ok)
+        box.pack()
+
+    def apply(self):
         pass
-    else:
-        print("Database Clear operation cancelled...")
-        sys.exit()
-    isdelete2 = input("Type 'COMPLETELYDELETE' to completely delete the entire database ==> ")
-    if isdelete2 == "COMPLETELYDELETE":
-        clearalldb()
-    else:
-        print("Database Clear operation cancelled...")
-        sys.exit()
+
+    def ok(self, event=None):
+        self.apply()
+        self.destroy()
+
+    def resize_event(self, event):
+        if self.winfo_exists():
+            self.update_image(event.width, event.height)
+
+def show_image_text_dialog(parent, title, image_path, text):
+    ImageTextDialog(parent, title, image_path, text)
+
+def run_operation(operation_number):
+    try:
+        if operation_number == 1:  # Insert Single Face
+            filepath = filedialog.askopenfilename(title="Select Image")
+            if not filepath:
+                return
+            faceencode = encode_face(filepath)
+            if str(faceencode) == "False":
+                log_output("No face detected in image or invalid image file!")
+                return
+            face_uuid = gen_id()
+            save_face(faceencode, face_uuid)
+            fullname = simple_input_dialog("Please input subject's Full Name")
+            new_face(face_uuid, fullname)
+            save_copy(filepath, face_uuid)
+            log_output(f"New face added to the database with ID: {face_uuid}, and Full Name: {fullname}")
+
+        elif operation_number == 2:  # Insert List of Faces
+            jsonfilemname = filedialog.askopenfilename(title="Select JSON File", filetypes=[("JSON Files", "*.json")])
+            if not jsonfilemname:
+                return
+            with open(jsonfilemname, 'r') as f:
+                peoples = json.load(f)
+
+            progress_bar["maximum"] = len(peoples)
+            progress_bar["value"] = 0
+            for i, people in enumerate(peoples):
+                log_output(f"Processing JSON file record number: [{i+1}]")
+                faceencode = encode_face(people['filepath'])
+                if str(faceencode) == "False":
+                    log_output(f"No face detected in image or invalid image file!, Full Name: {people['full_name']}")
+                    return
+                face_uuid = gen_id()
+                save_face(faceencode, face_uuid)
+                fullname = people['full_name']
+                new_face(face_uuid, fullname)
+                save_copy(people['filepath'], face_uuid)
+                log_output(f"New face added with ID: {face_uuid}, Full Name: {fullname}")
+                progress_bar["value"] = i + 1
+                root.update_idletasks()
+
+        elif operation_number == 3:  # Query Face from Image
+            filename = filedialog.askopenfilename(title="Select Image")
+            if not filename:
+                return
+            facencode = encode_face(filename)
+            if str(facencode) == "False":
+                log_output("No face detected in image or invalid image file!")
+                return
+
+            faces_dir = os.listdir("facedb")
+            progress_bar["maximum"] = len(faces_dir)
+            progress_bar["value"] = 0
+            resultfound = False
+            log_output("Searching db for similar faces...")
+            for i, facedir in enumerate(faces_dir):
+                with open("facedb/"+facedir, 'rb') as f:
+                    currencode = pickle.load(f)
+                if comparewithtuple(filename, currencode):
+                    log_output(f"Matched with Face ID:{facedir[:-5]}, Searching for full name...")
+                    log_output(f"Matched Face ID: {facedir[:-5]} with Full Name: {search_fullname(facedir[:-5])[0]['fullname']}")
+                    log_output(f"Database face image on path: imgfacedb/{facedir[:-5]}.png")
+                    show_image_text_dialog(root, "Candidate Data", f"imgfacedb/{facedir[:-5]}.png", "Full Name: "+search_fullname(facedir[:-5])[0]['fullname'])
+                    resultfound = True
+                    break
+                progress_bar["value"] = i + 1
+                root.update_idletasks()
+
+            if not resultfound:
+                log_output("No result found after going through the entire database!")
+
+        elif operation_number == 4:  # Query Face by ID
+            faceid = simple_input_dialog("Input Face ID")
+            if not faceid:
+                return
+
+            faces_dir = os.listdir("facedb")
+            progress_bar["maximum"] = len(faces_dir)
+            progress_bar["value"] = 0
+            for i, facedir in enumerate(faces_dir):
+                if faceid == facedir[:-5]:
+                    fullname = search_fullname(faceid)[0]['fullname']
+                    log_output(f"Full Name: {fullname}")
+                    log_output(f"Raw face image path: faceimgdb/{faceid}.png")
+                    show_image_text_dialog(root, "Candidate Data", f"imgfacedb/{faceid}.png", f"Full Name: {fullname}")
+                    break
+                progress_bar["value"] = i + 1
+                root.update_idletasks()
+
+            log_output("No record found for the given ID!")
+
+        elif operation_number == 5:  # Delete Face by Image
+            filename = filedialog.askopenfilename(title="Select Image")
+            if not filename:
+                return
+            facencode = encode_face(filename)
+            if str(facencode) == "False":
+                log_output("No face detected in image or invalid image file!")
+                return
+
+            faces_dir = os.listdir("facedb")
+            progress_bar["maximum"] = len(faces_dir)
+            progress_bar["value"] = 0
+            resultfound = False
+            log_output("Searching db for similar faces...")
+            for i, facedir in enumerate(faces_dir):
+                with open("facedb/"+facedir, 'rb') as f:
+                    currencode = pickle.load(f)
+                if comparewithtuple(filename, currencode):
+                    log_output(f"Matched with Face ID:{facedir[:-5]}, Searching for full name...")
+                    log_output(f"Matched Face ID: {facedir[:-5]} with Full Name: {search_fullname(facedir[:-5])[0]['fullname']}")
+                    resultfound = True
+                    log_output("Deleting data related to the detected face...")
+                    deleteface(facedir[:-5])
+                    log_output("Successfully deleted all data related to the face!")
+                    break
+                progress_bar["value"] = i + 1
+                root.update_idletasks()
+
+            if not resultfound:
+                log_output("No result found after going through the entire database!")
+
+        elif operation_number == 6:  # Delete Face by ID
+            faceid = simple_input_dialog("Input Face ID to delete")
+            if not faceid:
+                return
+            deleteface(faceid)
+            log_output("Successfully deleted all data related to the face!")
+
+        elif operation_number == 7:  # Clear Entire Database
+            confirmation = messagebox.askyesno("Dangerous Operation", "This will completely delete the entire database. Are you sure?")
+            if confirmation:
+                confirmation2 = simple_input_dialog("Type 'COMPLETELYDELETE' to confirm")
+                if confirmation2 == "COMPLETELYDELETE":
+                    clearalldb()
+                    log_output("Database cleared successfully.")
+                else:
+                    log_output("Database clear operation cancelled.")
+            else:
+                log_output("Database clear operation cancelled.")
+
+    except Exception as e:
+        log_output(f"Error: {str(e)}")
+
+def log_output(message):
+    print(message)
+    log_area.insert(tk.END, message + "\n")
+    log_area.see(tk.END)
+
+def simple_input_dialog(prompt):
+    return simpledialog.askstring("Input", prompt, parent=root)
+
+# Tkinter UI Setup
+root = tk.Tk()
+root.title("FaceDentify")
+
+frame = tk.Frame(root)
+frame.pack(padx=10, pady=10)
+
+operations = [
+    (1, "Insert Single Face"),
+    (2, "Insert List of Faces"),
+    (3, "Query Face from Image"),
+    (4, "Query Face by ID"),
+    (5, "Delete Face by Image"),
+    (6, "Delete Face by ID"),
+    (7, "Clear Entire Database")
+]
+
+for number, operation in operations:
+    btn = tk.Button(frame, text=f"{number}. {operation}", width=30, command=lambda num=number: run_operation(num))
+    btn.pack(pady=5)
+
+progress_bar = ttk.Progressbar(root, orient="horizontal", length=400, mode="determinate")
+progress_bar.pack(pady=10)
+
+log_area = tk.Text(root, height=10, width=50, state="normal")
+log_area.pack(padx=10, pady=10)
+
+check_filestrcture()
+root.mainloop()
