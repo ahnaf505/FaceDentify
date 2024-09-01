cc6c9a4f22cbcd194add90eddb9e69aec0652622
Ahnaf <159417805+ahnaf505@users.noreply.github.com>
2024-09-01 15:03:12 +0700

    main.py modified

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-print("Initializing...")
-from db import *
-import pickle
+import os
 import sys
 import json
+import pickle
+from db import *
 from compare import *
-isvalidfile = check_filestrcture()
-if isvalidfile:
-    print("Proper file structure detected!")
-else:
+from tqdm import tqdm
+
+print("Initializing...")
+if not check_filestrcture():
     print("Invalid file structure detected!")
     print("Fixing file structure...")
 
-# Simple debug menu.
 print("FaceDentify\n")
 print("1. Insert new single face into database")
-print("2. Insert a lists of faces into database")
+print("2. Insert a list of faces into database")
 print("3. Query face from biometric recognition")
 print("4. Query face from Face ID")
 print("5. Delete face from biometric recognition")
 print("6. Delete face from Face ID")
 print("7. Clear entire database CAUTION!!! REALLY DANGEROUS OPERATION")
 
-# User menu input field.
 menu = input("Please select menu ==> ")
 
 if menu == "1":
     filepath = input("\nInput image candidate filename ==> ")
     faceencode = encode_face(filepath)
-    if str(faceencode) == "False":
+    if faceencode is None or not faceencode.any():
         print("No face detected in image or invalid image file!")
         sys.exit()
     face_uuid = gen_id()
@@ -57,88 +55,106 @@ elif menu == "2":
     with open(jsonfilemname, 'r') as f:
         peoples = json.load(f)
     
+    total_records = len(peoples)
     for i, people in enumerate(peoples):
-        print(f"Processing json file record number: [{i+1}]")
+        sys.stdout.write(f"\rProcessing json file record number: [{i+1}/{total_records}]")
+        sys.stdout.flush()
         faceencode = encode_face(people['filepath'])
-        if str(faceencode) == "False":
-            print(f"No face detected in image or invalid image file!, Full Name: {people['full_name']}")
-            sys.exit()
+        if faceencode is None or not faceencode.any():
+            print(f"\nNo face detected in image or invalid image file!, Full Name: {people['full_name']}")
+            continue
         face_uuid = gen_id()
         save_face(faceencode, face_uuid)
         fullname = people['full_name']
         new_face(face_uuid, fullname)
         save_copy(people['filepath'], face_uuid)
         print(f"\nNew face added to the database with ID: {face_uuid}, and Full Name: {fullname}")
+
 elif menu == "3":
     filename = input("\nInput candidate image file name ==> ")
     facencode = encode_face(filename)
-    if str(facencode) == "False":
+    if facencode is None or not facencode.any():
         print("No face detected in image or invalid image file!")
         sys.exit()
     
     resultfound = False
     faces_dir = os.listdir("facedb")
     print("Searching db for similar faces...")
-    for facedir in faces_dir:
-        with open("facedb/"+facedir, 'rb') as f:
-            currencode = pickle.load(f)
-        if comparewithtuple(filename, currencode) == True:
-            print(f"Matched with Face ID:{facedir[:-5]} Searching for full name...")
-            print(f"Matched Face ID: {facedir[:-5]} with Full Name: {search_fullname(facedir[:-5])[0]['fullname']}")
-            print(f"Database face image on path: imgfacedb/{facedir[:-5]}.png")
-            resultfound = True
-            break
-    if resultfound == False:
-        print("No result found after going through the entire database!")
     
+    with tqdm(faces_dir, desc="Progress", unit="face") as pbar:
+        for facedir in pbar:
+            with open(f"facedb/{facedir}", 'rb') as f:
+                currencode = pickle.load(f)
+            if comparewithtuple(filename, currencode):
+                face_id = facedir[:-5]
+                fullname = search_fullname(face_id)[0]['fullname']
+                pbar.close()
+
+                print(f"\nMatched with Face ID: {face_id} Searching for full name...")
+                print(f"Matched Face ID: {face_id} with Full Name: {fullname}")
+                print(f"Database face image on path: imgfacedb/{face_id}.png")
+                resultfound = True
+                break
+    
+    if not resultfound:
+        print("\nNo result found after going through the entire database!")
+
 elif menu == "4":
     faceid = input("Input Face ID ==> ")
-    if search_fullname(faceid):
-        print(f"Full Name: {search_fullname(faceid)[0]['fullname']}")
-        print(f"Raw face image path: faceimgdb/{search_fullname(faceid)[0]['faceid']}.png/jpg")
+    record = search_fullname(faceid)
+    if record:
+        fullname = record[0]['fullname']
+        print(f"Full Name: {fullname}")
+        print(f"Raw face image path: faceimgdb/{record[0]['faceid']}.png/jpg")
     else:
         print("No record found from the given ID!")
 
 elif menu == "5":
     filename = input("\nInput candidate image file name ==> ")
     facencode = encode_face(filename)
-    if str(facencode) == "False":
+    if facencode is None or not facencode.any():
         print("No face detected in image or invalid image file!")
         sys.exit()
     
     resultfound = False
     faces_dir = os.listdir("facedb")
     print("Searching db for similar faces...")
-    for facedir in faces_dir:
-        with open("facedb/"+facedir, 'rb') as f:
-            currencode = pickle.load(f)
-        if comparewithtuple(filename, currencode) == True:
-            print(f"Matched with Face ID:{facedir[:-5]} Searching for full name...")
-            print(f"Matched Face ID: {facedir[:-5]} with Full Name: {search_fullname(facedir[:-5])[0]['fullname']}")
-            resultfound = True
-            print("Deleting data related to the detected face...")
-            deleteface(facedir[:-5])
-            print("Succesfully deleted all data related to the face!")
-            break
-    if resultfound == False:
-        print("No result found after going through the entire database!")
+    
+    with tqdm(total=len(faces_dir), desc="Progress", unit="face") as pbar:
+        for facedir in faces_dir:
+            with open(f"facedb/{facedir}", 'rb') as f:
+                currencode = pickle.load(f)
+    
+            if comparewithtuple(filename, currencode):
+                face_id = facedir[:-5]
+                fullname = search_fullname(face_id)[0]['fullname']
+    
+                pbar.close()
+                print(f"\nMatched with Face ID: {face_id}. Searching for full name...")
+                print(f"Matched Face ID: {face_id} with Full Name: {fullname}")
+                print("Deleting data related to the detected face...")
+    
+                deleteface(face_id)
+    
+                print("Successfully deleted all data related to the face!")
+                resultfound = True
+                break
+            
+    if not resultfound:
+        print("\nNo result found after going through the entire database!")
 
 elif menu == "6":
-    faceid= input("Input Face ID to delete ==> ")
+    faceid = input("Input Face ID to delete ==> ")
     deleteface(faceid)
-    print("Succesfully deleted all data related to the face!")
+    print("Successfully deleted all data related to the face!")
 
 elif menu == "7":
     print("REALLY DANGEROUS OPERATION, THIS CANNOT BE UNDONE.")
-    isdelete1 = input("Please type '9840nfd9' to continue ==> ")
-    if isdelete1 == "9840nfd9":
-        pass
-    else:
+    if input("Please type '9840nfd9' to continue ==> ") != "9840nfd9":
         print("Database Clear operation cancelled...")
         sys.exit()
-    isdelete2 = input("Type 'COMPLETELYDELETE' to completely delete the entire database ==> ")
-    if isdelete2 == "COMPLETELYDELETE":
+    if input("Type 'COMPLETELYDELETE' to completely delete the entire database ==> ") == "COMPLETELYDELETE":
         clearalldb()
+        print("Database cleared.")
     else:
         print("Database Clear operation cancelled...")
-        sys.exit()
